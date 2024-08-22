3d0e225ba11369bcfd3e4350d3638487f8c02018
Dmitry <dymitr.paremski@thesis.co>
2024-08-22 11:00:42 +0200

    Adding custom precompiled support
    These changes were migrated from EVMOS v1.10.26-evmos-rc2 tag. Relevant
    precompile EVMOS commits that were part of this tag:
    
    - 8d407912cad95d41db1e472f35a1eba6dc7dc363
    - fcf5e42ce33b315dc294d200ad0c3da96fbc441f
    - f24eefdf82c19088c36fee898d66370a8489c9e7
    - 359caee7e31063a6fa8a01832cabe0a35d383fff
    - d7a659397e07fca3a3516851aa8feefc0d632f1d
    
    The above changes were added on top of the latest available go-ethereum
    tag which is v1.14.8 Now we have the latest go-ethereum code with all
    the latest fixes along with the EVMOS like custom precompile support.

--- eth/tracers/native/call_flat.go
+++ eth/tracers/native/call_flat.go
@@ -207,7 +207,7 @@ func (t *flatCallTracer) OnTxStart(env *tracing.VMContext, tx *types.Transaction
 	t.tracer.OnTxStart(env, tx, from)
 	// Update list of precompiles based on current block
 	rules := env.ChainConfig.Rules(env.BlockNumber, env.Random != nil, env.Time)
-	t.activePrecompiles = vm.ActivePrecompiles(rules)
+	t.activePrecompiles = vm.DefaultActivePrecompiles(rules)
 }
 
 func (t *flatCallTracer) OnTxEnd(receipt *types.Receipt, err error) {
